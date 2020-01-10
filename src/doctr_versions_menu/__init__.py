3e069356dbd99b8d8fdf9d985d8a22e5e7d5c8b1
Michael Goerz <goerz@stanford.edu>
2020-01-10 04:24:26 -0500

    Bump version to 0.1.0-rc2+dev

--- src/doctr_versions_menu/__init__.py
+++ src/doctr_versions_menu/__init__.py
@@ -1,6 +1,6 @@
 """Doctr Versions Menu."""
 
-__version__ = '0.1.0-rc2'
+__version__ = '0.1.0-rc2+dev'
 
 # All members whose name does not start with an underscore must be listed
 # either in __all__ or in __private__
