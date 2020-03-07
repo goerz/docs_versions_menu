0e8c02185d17a9ca91da3e9b39d63455e933d333
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-03-07 20:13:41 +0000

    Add versions.py script

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0+dev (6e616b7)',
+    VERSION: '0.1.0+dev (680b60e)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
