a6e4e97b3736114ce3fdec15696d1180769fead2
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-12-10 17:29:12 +0000

    Update docs after building Travis build 165 of goerz/doctr_versions_menu
    The docs were built from the branch 'master' against the commit
    2cddf213e9e76bea482482b85308d57eeb52ed4e.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/goerz/doctr_versions_menu/jobs/457486785.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.0/lib/python3.8/site-packages/doctr/__main__.py deploy --key-path docs/doctr_deploy_key.enc --command=doctr-versions-menu --built-docs docs/_build/html --no-require-master --build-tags master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+dev (ac31fda)',
+    VERSION: '0.3.0+dev (2cddf21)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
