cb191a151c969e64096db6e70478ac0929d34a3e
Michael Goerz <goerz@stanford.edu>
2021-04-04 18:06:14 -0400

    Bump version to 0.5.0-dev

--- src/docs_versions_menu/__init__.py
+++ src/docs_versions_menu/__init__.py
@@ -4,7 +4,7 @@ This package does not define a public API: it publicly provides only a Sphinx
 extension and a command line program.
 """
 
-__version__ = '0.5.0-rc2'
+__version__ = '0.5.0-dev'
 
 # All members whose name does not start with an underscore must be listed
 # either in __all__ or in __private__
